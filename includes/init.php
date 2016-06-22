d9ee2168cfd7331f86dc962e444602b4bd620526
dave <dunnedave+coursera@gmail.com>
2016-06-21 18:56:36 -0700

    Updated init files

--- includes/init.php
+++ includes/init.php
@@ -4,7 +4,7 @@ defined('DS') ? null : define('DS' , DIRECTORY_SEPARATOR);
 if ($_SERVER['SERVER_NAME'] == "sandbox.dev"){
 	defined('SITE_ROOT') ? null : define('SITE_ROOT' , DS.'media'.DS.'sf_sandbox'.DS.'davedunnephoto.com'.DS);
 } else {
-	defined('SITE_ROOT') ? null : define('SITE_ROOT' , '/home2/davedunn/');
+	defined('SITE_ROOT') ? null : define('SITE_ROOT' , '/home2/xxx/');
 }
 
 defined('LIB_PATH') ? null : define('LIB_PATH' , SITE_ROOT.DS.'includes');
